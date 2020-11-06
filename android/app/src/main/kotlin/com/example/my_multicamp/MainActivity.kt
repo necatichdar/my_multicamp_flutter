00f6747239669fece9884c9e534c9f17f8d1c71d
Necati Çuhadar <necaticuhadar06@gmail.com>
2020-11-06 21:20:44 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/my_multicamp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_multicamp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dart/dart_temelleri.dart
@@ -0,0 +1,21 @@
+void main() {
+  /*
+  var
+  final  //Degistirilemez
+  const
+  const'un degeri programi calismadan once belli olmali, calisirken degismez.
+
+  dynamic : herhangi tip vermezsek dynamic olarak calisir. Herhangi bisey olabilir demektir.
+  final myList = [];
+
+  Degiskenler 
+  int : Tam Sayi
+  double : Noktali Sayilar 
+  String : Text icin
+  bool : true veya false
+
+
+
+
+*/
+}
