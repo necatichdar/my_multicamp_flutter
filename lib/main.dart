b339f761d3f5e7afc91b6d10b663e82ff7248875
Necati Çuhadar <necaticuhadar06@gmail.com>
2020-11-11 22:27:00 +0300

    11.11.2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MulticampApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    builder: (context, child) => MultiCampApp(),
+  ));
 }
 
-class MulticampApp extends StatefulWidget {
+class MultiCampApp extends StatefulWidget {
   @override
-  _MulticampAppState createState() => _MulticampAppState();
+  _MultiCampAppState createState() => _MultiCampAppState();
 }
 
-class _MulticampAppState extends State<MulticampApp> {
+class _MultiCampAppState extends State<MultiCampApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Multicamp App"),
-      ),
-      body: ListView.builder(itemBuilder: (_, int index) {
+        appBar: AppBar(
+          title: Text("Multicamp App"),
+        ),
+        body: GridView.builder(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.all(10),
+              child: Card(
+                color: Colors.red,
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Image.network(
+                          "https://387b72ivq1z3mni0n261y7l1-wpengine.netdna-ssl.com/wp-content/uploads/2018/03/success-story-hamilton-app-innovation-min.png"),
+                    ),
+                    Text(
+                      "Music Item $index",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Text(
+                      "Music Item $index",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ));
+  }
+}
+
+class listviewBuilder extends StatelessWidget {
+  const listviewBuilder({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, int index) {
         return ListTile(
           title: Text("Music $index"),
+          subtitle: Text("Subtitle $index"),
+          leading: Text("Leading $index"),
+          trailing: Text("Trailing $index"),
         );
-      }),
+      },
     );
   }
 }
